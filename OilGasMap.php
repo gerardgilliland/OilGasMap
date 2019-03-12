3e623afe4bcb77c84b41a7a0808e916aa551efe4
gerardgilliland <gerardg@modelsw.com>
2019-03-11 20:58:39 -0600

    Added Options for AutoRefresh, Audible Warnings and Alarms
    Also removed UpdateMap() to LoadMonitor

--- OilGasMap.php
+++ OilGasMap.php
@@ -10,111 +10,147 @@
 
 <!DOCTYPE html >
 	<head>
-		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-		<title>Gas and Oil Map</title>
-		<style>
-		/* Always set the map height explicitly to define the size of the div
-		* element that contains the map. */
-		#map {
-			height: 100%;
-		}
-		/* Optional: Makes the sample page fill the window. */
-		html, body {
-			height: 100%;
-			margin: 0;
-			padding: 0;
-		}
-		</style>
-<?php	
+	<!--<meta http-equiv="refresh" content="60"/>-->
+	<!--<meta name="description" content="<?php echo $meta1; ?>">-->
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+	<title>Citizen's Monitor: Map</title>
+	<style>
+	/* Always set the map height explicitly to define the size of the div
+	* element that contains the map. */
+	#map {
+		height: 100%;
+	}
+	/* Optional: Makes the sample page fill the window. */
+	html, body {
+		height: 100%;
+		margin: 0;
+		padding: 0;
+	}
+	</style>
+<?php
 	global $link_id;
 	$link_id = db_connect();
-	UpdateMap();
-	
-	Function updateMap() {		
-		global $link_id;
-		$qry = "SELECT Max(Loc) as maxLoc FROM tblLocation";
-		$rs = mysqli_query($link_id, $qry);
-		$k = mysqli_num_rows($rs); // it will be one row
-		mysqli_data_seek($rs, $k);
-		$row = mysqli_fetch_row($rs); 
-		$maxLoc = $row[0];
-		//echo "maxLoc=$maxLoc<br>";
-		$XmlName = "OilGasMarkers.xml";
-		$fpx = fopen($XmlName, "w"); // For Output 
-		$nl = chr(13) . chr(10);		
-		fwrite($fpx, "<?xml version=\"1.0\" ?>" . $nl);
-		fwrite($fpx, "<markers>" . $nl);		
-		for ($id = 1; $id <= $maxLoc; $id++) {			
-			$qry = "SELECT * FROM tblLocation WHERE Loc = $id";
-			$rs = mysqli_query($link_id, $qry);
-			$k = mysqli_num_rows($rs);
-			//echo "id=$id k=$k <br>";
-			if ($k == 1) {
-				mysqli_data_seek($rs, $k);
-				$row = mysqli_fetch_row($rs); 
-				$lat = $row[1];
-				$lng = $row[2];
-				//echo "id=$id lat=$lat lng=$lng <br>"; 
-				$qry = "SELECT MAX(Date) FROM tblMonitor WHERE Loc = $id";
-				$rs = mysqli_query($link_id, $qry);
-				$i = mysqli_num_rows($rs);
-				if ($i == 1) {  // should be one only row
-					mysqli_data_seek($rs, $i);
-					$row = mysqli_fetch_row($rs); 
-					$date = $row[0];
-					$qry = "SELECT * FROM tblMonitor WHERE Loc = $id and Date = '$date'";
-					$rs = mysqli_query($link_id, $qry);
-					$j = mysqli_num_rows($rs);
-					if ($j == 1) {
-						mysqli_data_seek($rs, $j);
-						$row = mysqli_fetch_row($rs); 
-						// tblMonitor (Loc, Date, PM1, PM25, PM10, Temp, Press, RH, Ohms, Quality, VOC, Methane, SoundDb, Freq)
-						$loc = $row[0];
-						$date = $row[1];
-						fwrite($fpx, "<marker id=\"$row[0]\"" );
-						fwrite($fpx, " lat=\"$lat\"");
-						fwrite($fpx, " lng=\"$lng\"");
-						fwrite($fpx, " date=\"$row[1]\"");
-						fwrite($fpx, " pm1=\"$row[2]\"");
-						fwrite($fpx, " pm25=\"$row[3]\"");
-						fwrite($fpx, " pm10=\"$row[4]\"");
-						fwrite($fpx, " temp=\"$row[5]\""); 						
-						fwrite($fpx, " press=\"$row[6]\""); 						
-						fwrite($fpx, " rh=\"$row[7]\""); 
-						fwrite($fpx, " qual=\"$row[9]\"");
-						fwrite($fpx, " voc=\"$row[10]\"");						
-						//fwrite($fpx, " ch4=\"$row[11]\""); 
-						fwrite($fpx, " db=\"$row[12]\"");
-						fwrite($fpx, " hz=\"$row[13]\"");
-						$type = "I";
-						if ($row[10] > 10 || $row[11] > 200 || $row[9] < 80) {
-							$type = "W";
-						} elseif ($row[10] > 100 || $row[11] > 500 || $row[9] < 70) {
-							$type = "A";
-						}
-						fwrite($fpx, " type=\"$type\" />" . $nl);
-					}
-				}	
-			}		
-		}		
-		fwrite($fpx, "</markers>" . $nl);
-		//echo "write markers<br>";
+	global $refresh, $warning, $alarm, $refreshMinutes;
+	global $last;
+	$refresh = $_COOKIE["refresh"];
+	$refreshMinutes = $_COOKIE["refreshMinutes"]; 
+	if ($refreshMinutes == null) {
+		$refreshMinutes = 1;
 	}
+	$warning = $_COOKIE["warning"];
+	$alarm = $_COOKIE["alarm"];
+	$qry = "Select Max(Loc) from tblLocation";
+	$rs = mysqli_query($link_id, $qry);
+	$i = mysqli_num_rows($rs);
+	$row = mysqli_fetch_row($rs); 
+	$last = $row[0] + 1;
+	//echo "refresh:$refresh minutes:$refreshMinutes warning:$warning alarm:$alarm<br>";
 ?>
-	<script>
-		<!-- setInterval(function(){ alert("Hello"); }, 3000); -->
-		<!-- UpdateMap(); -->
-	</script>
-	
 	</head>
 
-	<body>
-		<link rel="stylesheet" type="text/css" href="msStyle.css" />
+	<body onload="currentCookies()"> 
+		<!-- onload is actually afterload -->
+ 		<link rel="stylesheet" type="text/css" href="msStyle.css" />
+		<audio id="monitor_wrn">
+			<source src="beep-07.mp3" type="audio/mpeg">
+		</audio>
+		<audio id="monitor_alm">
+			<source src="beep-beep.mp3" type="audio/mpeg">
+		</audio>
 		<div id="map"></div>
 
 		<script>
+		var refresh = <?php echo $refresh ?>;
+		var refreshMinutes = <?php echo $refreshMinutes ?>;
+		if (refresh == true) {
+			var refreshInterval = refreshMinutes * 60000;
+			setTimeout(function () {
+				updateMap();
+			}, refreshInterval); 
+		}
+		var warning = <?php echo $warning ?>;
+		var alarm = <?php echo $alarm ?>;
+		var last = <?php echo $last ?>;
+	    var existing = [];
+	    var current = [];
+		//https://www.soundjay.com/beep-sounds-1.html
+		var audio_wrn = document.getElementById("monitor_wrn");
+		var audio_alm = document.getElementById("monitor_alm");
 		
+		function currentCookies() {
+			//lst = "compare cookies: \n";	
+			//lst += "existing:" + existing + "\n";
+			//lst += "current:" + current + "\n";
+			//alert (lst);
+			var i;
+			var cntA = 0;
+			var cntW = 0;
+			for (i = 1; i<last; i++) {
+				if (alarm == true) {
+					if (existing[i] == "I" && current[i] == "A") {
+						//alert (i + " was information now alarm")
+						cntA++;
+						// add to log
+					}
+					if (existing[i] == "W" && current[i] == "A") {
+						//alert (i + " was warning now alarm")
+						cntA++;
+						// add to log
+					}
+				}
+				if (warning == true) {
+					if (existing[i] == "I" && current[i] == "W") {
+						//alert (i + " was information now warning")
+						cntW++;
+						// add to log
+					}
+				}
+			}
+
+			if (cntA > 0) {
+				playAlarm();
+			} else {
+				if (cntW > 0) {
+					playWarning();
+				}
+			}
+		}
+
+		function updateMap() {
+			location.reload();	
+		}	
+		
+		function existingCookies() {
+			//var lst = "existing cookies: \n";	
+			var i;
+			for (i = 1; i<last; i++) {
+				var cname = "status" + i;
+				var cx = getCookie(cname);
+				existing[i] = cx;
+				//lst += "existing:" + cx + "\n";	
+            }	
+			//alert (lst);
+		}
+
+
+		function getCookie(cname) {
+			var name = cname + "=";
+			//alert ("name:" + name);
+			var decodedCookie = decodeURIComponent(document.cookie);
+			var ca = decodedCookie.split(';');
+			//alert ("ca.length:" + ca.length);
+			for(var i = 0; i <ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt(0) == ' ') {
+					c = c.substring(1);
+				}
+				if (c.indexOf(name) == 0) {
+					return c.substring(name.length, c.length);
+				}
+			}
+			return "";
+		}
 		var customLabel = {
 			W: {
 				label: 'W'
@@ -124,10 +160,22 @@
 			},
 			I: {
 				label: 'I'
+			},  
+			X: {
+				label: 'X'
 			}  
+			
 		};
 		
-	
+		function playAlarm() {
+			audio_alm.play();
+		}
+
+		function playWarning() {
+			audio_wrn.play();
+		}
+
+
 		function initMap() {
 			var map = new google.maps.Map(
 				document.getElementById('map'), {
@@ -136,7 +184,9 @@
 				}
 			);
 			var infoWindow = new google.maps.InfoWindow;
+
 			// Change this depending on the name of your PHP or XML file
+			// NOTE: the warnings and alarms have been moved to LoadMonitor.php -- See UpdateMap()
 			downloadUrl('https://www.modelsw.com/OilGasMonitor/OilGasMarkers.xml', function(data) {
 				var xml = data.responseXML;
 				var markers = xml.documentElement.getElementsByTagName('marker');
@@ -149,6 +199,8 @@
 					var temp = markerElem.getAttribute('temp');
 					var press = markerElem.getAttribute('press');
 					var rh = markerElem.getAttribute('rh');
+					var wdir = markerElem.getAttribute('wdir');
+					var wspd = markerElem.getAttribute('wspd');
 					var db = markerElem.getAttribute('db');
 					var hz = markerElem.getAttribute('hz');
 					var pm1 = markerElem.getAttribute('pm1');
@@ -158,8 +210,13 @@
 					var point = new google.maps.LatLng(
 						parseFloat(markerElem.getAttribute('lat')),
 						parseFloat(markerElem.getAttribute('lng')));
+
+					if (id > 0) {
+						document.cookie="status" + id + "=" +type+";max-age=86400;"; // one day
+						current[id] = type;
+					}
+					
 					var infowincontent = document.createElement('div');
-		
 				    var btn = document.createElement('button');
 					var btntxt = document.createTextNode('Plot');
 					btn.appendChild(btntxt);
@@ -167,9 +224,13 @@
 						plotid(id);
 					};
 					infowincontent.appendChild(btn);
+					var text = document.createElement('text');
+					text.textContent = " Citizen's Monitor";			
+					infowincontent.appendChild(text);
+			
 					infowincontent.appendChild(document.createElement('br'));
 					
-					var text = document.createElement('text');
+					text = document.createElement('text');
 					text.textContent = " id = " + id + ", date = " + dat;			
 					infowincontent.appendChild(text);
 					infowincontent.appendChild(document.createElement('br'));
@@ -181,6 +242,17 @@
 					
 					text = document.createElement('text');
 					text.textContent = " temp = " + temp + ", Pa = " + press + ", rh = " + rh;
+					infowincontent.appendChild(text);
+					infowincontent.appendChild(document.createElement('br'));	
+					
+					text = document.createElement('text');
+					if (wspd.length == 1) {
+						wspd = "0" + wspd;
+					}
+					var dot = ".";
+					var position = -1;
+					var wspdD10 = [wspd.slice(0, position), dot, wspd.slice(position)].join('');
+					text.textContent = " wind dir = " + wdir + " spd = " + wspdD10;
 					infowincontent.appendChild(text);
 					infowincontent.appendChild(document.createElement('br'));	
 					
@@ -207,34 +279,74 @@
 				});
 			});
         }
+
+		
 		function plotid(id) {
-			<!-- alert('plot id:' + id); -->
-			<!-- window.open("<a href='Plotid.html#'<?php id ?> target='_blank'></a>") -->
-			<!-- window.open("https://www.modelsw.com/OilGasMonitor/PlotId.html#1" target="_blank") -->
 			window.location.href = "https://www.modelsw.com/OilGasMonitor/PlotId.php?id="+id;
-			
 		}
+
+
 		function downloadUrl(url, callback) {
+			existingCookies();  // read the cookies before you load the map
 			var request = window.ActiveXObject ?
 				new ActiveXObject('Microsoft.XMLHTTP') :
 				new XMLHttpRequest;
+
 			request.onreadystatechange = function() {
 				if (request.readyState == 4) {
 					request.onreadystatechange = doNothing;
 					callback(request, request.status);
 				}
 			};
+
 			request.open('GET', url, true);
 			request.send(null);
 		}
+
+
 		function doNothing() {}
 		</script>
 		<script async defer
-			src="https://maps.googleapis.com/maps/api/js?key=ContactMe&callback=initMap">
+			src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_MCmPQPSTE0eNY8zXEGzkmziLFQx_IMM&callback=initMap">
 		</script>
 
-<?php
-    include "./footer.inc";
-?>
+		
+	<form name="OilGasMap" id="OilGasMap">
+		
+	<table width = "100%">
+		<tr><td colspan = "5"><hr /></td>
+		<tr><td><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src=" https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>23Jun2017</td>
+		<td><input type="checkbox" name="refresh" value="refresh" <?php if ($refresh==true) { echo "checked"; } ?> onClick="activeRefresh(checked)"> refresh 
+		    <input type="text" name="refreshMinutes" size = "4" value="<?php echo $refreshMinutes ?>" onChange="activeMinutes(this.value)"> minutes</td>
+		<td><input type="checkbox" name="warning" value="warning" <?php if ( $warning==true) { echo "checked"; } ?> onClick="activeWarning(checked)"> warning </td>
+		<td><input type="checkbox" name="alarm" value="alarm" <?php if ( $alarm==true) { echo "checked"; } ?> onClick="activeAlarm(checked)"> alarm </td>
+		<td style="float:right"><a href = "index.html" target="_top">Return to Homepage</a>
+		<img src="Images/x.jpg" width = "33px" height = "38px" /></a></td></tr>
+		<tr><td colspan = "5">This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a> &nbsp; &nbsp; <?php echo Date("Y:m:d H:i:s") ?> </td></tr>
+    </table>      		
+	</form>
+	
 	</body>
+<script>
+	function activeRefresh(checked) {
+		document.cookie="refresh="+checked+";max-age=604800;";
+        location.reload();  // will force the timeout to start (if true) or stop (if false)
+		//alert ("refresh:" + checked);
+	}
+	function activeMinutes(val) { 	
+		var strmin = val.toString();
+	    document.cookie="refreshMinutes="+strmin+";max-age=604800;"; 	
+		//location.reload();  // will force the timeout to start (if true) or stop (if false)
+	}
+	function activeWarning(checked) { 
+		document.cookie="warning="+checked+";max-age=604800;";
+		//location.reload();
+		//alert ("warning:" + checked);
+	}
+	function activeAlarm(checked) { 
+		document.cookie="alarm="+checked+";max-age=604800;";
+		//location.reload();
+		//alert ("alarm:" + checked);
+	}
+</script>
 </html>
